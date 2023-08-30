5817e834a4554917b90d88f20b352b5a06eac0ab
Ashish-kumar0707 <ashishkumar6378269775@gmail.com>
2023-08-30 16:44:25 +0530

    fixed the image

--- components/Mainpage.jsx
+++ components/Mainpage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Mainpage() {
   return (
     <div style={{ }}>
-        <div style={{ width:'400px' }}>
-          <img style={{ width:'400px' }} src="https://www.oberlo.com/media/1603967807-color-psychology.jpg?fit=max&fm=jpg&w=1824" alt="" />
+        <div style={{ maxWidth:'400px', minWidth:'300px' }}>
+          <img style={{ width:'100%' }} src="https://www.oberlo.com/media/1603967807-color-psychology.jpg?fit=max&fm=jpg&w=1824" alt="" />
         </div>
         {/* <p>hellon</p> */}
     </div>
